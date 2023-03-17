8a2b6b54c298e6b6e052715f8ecd4101a4a027b9
thq919 <89457417+thq919@users.noreply.github.com>
2023-03-17 17:26:45 +1000

    Update number_trivia_repository.dart
    У тебя метод называется concrete, а принимает Null  в число

--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -4,6 +4,6 @@ import 'package:flutter_tdd/core/error/failures.dart';
 import 'package:flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int? number);
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
   Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
 }
