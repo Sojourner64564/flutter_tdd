e159a49487ed1f6b5e9e2cf12d5848de53a74cf4
Sojourner64564 <122652562+Sojourner64564@users.noreply.github.com>
2023-03-19 21:00:34 +0300

    loh

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,11 +1,24 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_tdd/core/error/failures.dart';
+import 'package:flutter_tdd/core/platform/network_info.dart';
+import 'package:flutter_tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:flutter_tdd/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  NumberTriviaRepositoryImpl(
+      @required this.remoteDataSource,
+      @required this.localDataSource,
+      @required this.networkInfo);
 
-@override
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+
+  @override
 Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int? number){
 return null;
 }
