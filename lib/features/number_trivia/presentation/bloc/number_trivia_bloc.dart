d515e629b06d20998d135ba7461c2752f4b01511
Sojourner64564 <122652562+Sojourner64564@users.noreply.github.com>
2023-03-26 02:32:06 +0300

    loh

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,17 +1,31 @@
 //import 'dart: async';
 import 'package:bloc/bloc.dart';
+import 'package:flutter_tdd/core/util/input_converter.dart';
+import 'package:flutter_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import './bloc.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc(super.initialState);
+  NumberTriviaBloc(
+    super.initialState,
+    GetConcreteNumberTrivia concrete,
+    GetRandomNumberTrivia random,
+    this.inputConverter,
+  )   : assert(concrete != null),
+        assert(random != null),
+        assert(inputConverter != null),
+        getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random;
 
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
 
   @override
-  NumberTriviaState get initialState => InitialNumberTriviaState();
+  NumberTriviaState get initialState => Empty();
 
   @override
   Stream<NumberTriviaState> mapEventToState(
-      NumberTriviaEvent event,
-      ) async* {
-  }
+    NumberTriviaEvent event,
+  ) async* {}
 }
